4641197f874ee47c89584b190a5c16599e390df9
Jasper Yu <007seadog@gmail.com>
2020-10-03 06:28:22 +0800

    Added some animation

--- src/scene/welcome.js
+++ src/scene/welcome.js
@@ -17,10 +17,13 @@ var SceneWelcome = new Phaser.Class({
 
 	create: function ()
 	{
-		var logo = this.add.image(700, 300, 'logo');
+		var logo = this.add.image(700, 350, 'logo');
+		this.tweens.add({'targets': logo, 'y': 250, 'duration': 1500});
 		var btn = this.add.image(700, 600, 'button');
+		btn.setAlpha(0.1)
+		this.tweens.add({'targets': btn, 'alpha': 1, 'duration': 2000});
 		btn.once('pointerdown', function (event) {
-			this.scene.transition({ target: 'sceneWelcomeVideo', duration: 0 });
+			this.scene.transition({ target: 'sceneWelcomeVideo', duration: 0});
 		}, this);
 		btn.setInteractive({ cursor: 'pointer' })
 	}
