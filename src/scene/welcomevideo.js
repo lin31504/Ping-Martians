27ff9d0e57f27cb1748ee2f738a73dc7e60716cb
Jasper Yu <007seadog@gmail.com>
2020-10-03 06:40:37 +0800

    Added more animation

--- src/scene/welcomevideo.js
+++ src/scene/welcomevideo.js
@@ -16,8 +16,10 @@ var SceneWelcomeVideo = new Phaser.Class({
 
 	create: function ()
 	{
-		video = this.add.video(400, 300, 'space').on('complete', () => {});
+		var video = this.add.video(400, 300, 'space').on('complete', () => {});
+		var t = this.add.text(950, 700, 'Some story\nMore Story\nStill Story\nA Long Story\nStory.....\nBoring Story\nCool Story\nIntresting Story', {'fontSize': '40px'})
 		video.play(true);
+		this.tweens.add({'targets': t, 'y': -500, 'duration': 8000}).on('complete', () => {video.destroy();});
 	}
 
 });
