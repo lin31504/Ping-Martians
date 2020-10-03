ef31f46865aa94e6777978ba7550774cbed1c192
Jasper Yu <007seadog@gmail.com>
2020-10-04 01:14:52 +0800

    Added apps

--- src/scene/storyA_1.js
+++ src/scene/storyA_1.js
@@ -13,29 +13,40 @@ var SceneStoryA1 = new Phaser.Class({
 	{
 		this.load.image('bg_StoryA1', 'assets/bg_StoryA1.png');
 		this.load.image('fs_app', 'assets/fs_app.png');
+		this.load.image('mb_app', 'assets/mb_app.png');
+		this.load.image('ob_app', 'assets/ob_app.png');
 		this.load.image('laptop', 'assets/laptop.png');
 		this.load.image('cactus', 'assets/cactus.png');
+		this.load.image('ad1', 'assets/screen_logo_1.png');
+		this.load.image('ad2', 'assets/screen_logo_2.png');
+		this.load.image('fg_StoryA1', 'assets/fg_storyA1_effect.png');
 	},
 
 	create: function ()
 	{
 		var bg_StoryA1 = this.add.image(0, 0, 'bg_StoryA1').setOrigin(0).setScale(0.711);
-		var feedbackSys = this.add.image(420, 410, 'fs_app').setScale(0.7);
+		var fsapp = this.add.image(440, 410, 'fs_app').setScale(0.7);
+		var mbapp = this.add.image(620, 410, 'mb_app').setScale(0.7);
+		var obspp = this.add.image(800, 410, 'ob_app').setScale(0.7);
 		var machine2 = this.add.image(350, 670, 'laptop').setScale(0.7);
 		var machine3 = this.add.image(1230, 640, 'cactus').setScale(0.7);
+		var ads = [];
+		ads.push(this.add.image(180, 330, 'ad1').setScale(0.7));
+		ads.push(this.add.image(180, 330, 'ad2').setScale(0.7));
+		ads[1].alpha = 0;
+		var fg_StoryA1 = this.add.image(0, 0, 'fg_StoryA1').setOrigin(0).setScale(0.711);
 
-		feedbackSys.on('pointerdown', function (event) {
-           this.scene.transition({ target: 'sceneBinaryGame', duration: 0});
+		fsapp.on('pointerdown', function (event) {
+			this.scene.transition({ target: 'sceneBinaryGame', duration: 0});
 		}, this);
-        machine2.on('pointerdown', function (event) {
+		machine2.on('pointerdown', function (event) {
 			this.scene.transition({ target: 'sceneMachine2', duration: 0});
 		}, this);
 		machine3.on('pointerdown', function (event) {
 			this.scene.transition({ target: 'sceneMachine3', duration: 0});
 		}, this);
-		feedbackSys.setInteractive({ cursor: 'pointer' })
+		fsapp.setInteractive({ cursor: 'pointer' })
 		machine2.setInteractive({ cursor: 'pointer' })
 		machine3.setInteractive({ cursor: 'pointer' })
-	}
-
+	},
 });
