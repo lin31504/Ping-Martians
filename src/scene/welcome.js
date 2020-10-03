cc35c02d65cd5e68570f4bfcabc3d59946427758
Jasper Yu <007seadog@gmail.com>
2020-10-04 01:46:36 +0800

    Added welcome animation

--- src/scene/welcome.js
+++ src/scene/welcome.js
@@ -12,19 +12,24 @@ var SceneWelcome = new Phaser.Class({
 	preload: function ()
 	{
 		this.load.image('logo', 'assets/logo.png');
-		this.load.image('button', 'assets/button.png');
+		this.load.image('hint', 'assets/button.png');
+		this.load.image('bg', 'assets/bg_welcome.png');
 	},
 
 	create: function ()
 	{
+		var bg = this.add.image(0, 0, 'bg').setOrigin(0).setScale(0.711);
 		var logo = this.add.image(700, 350, 'logo');
-		this.tweens.add({'targets': logo, 'y': 250, 'duration': 1500});
-		var btn = this.add.image(700, 600, 'button');
-		btn.setAlpha(0.1)
-		this.tweens.add({'targets': btn, 'alpha': 1, 'duration': 2000});
+		var hint = this.add.image(900, 600, 'hint');
+		var btn = this.add.rectangle(680, 620, 180, 200, 0xff0000, 0.0);
+		hint.setAlpha(0)
+		this.tweens.add({'targets': bg, 'y': -690, 'duration': 5000});
+		this.tweens.add({'targets': logo, 'y': 250, 'duration': 4000});
+		this.tweens.add({'targets': hint, 'alpha': 1, 'duration': 15000});
 		btn.once('pointerdown', function (event) {
-			//this.scene.transition({ target: 'sceneWelcomeVideo', duration: 0});
-			this.scene.transition({ target: 'sceneStoryA1', duration: 0});
+			//this.scene.transition({ target: 'sceneStoryA1', duration: 0});
+			this.tweens.add({targets: bg, 'scale': 1.3, 'x': -560, 'y': -1900, 'alpha': 0,'duration': 6000}).on('complete', () => {this.scene.transition({ target: 'sceneWelcomeVideo', duration: 0})})
+			this.tweens.add({'targets': [logo, hint], 'alpha': 0, 'duration': 1000});
 		}, this);
 		btn.setInteractive({ cursor: 'pointer' })
 	}
