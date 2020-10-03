24b941f0ab8b7999cc6854e6f2207eed7f14c5eb
thomasya <thomas_ya@outlook.com>
2020-10-03 01:49:52 +0100

    localstorage demo

--- src/scene/binarygame.js
+++ src/scene/binarygame.js
@@ -13,27 +13,41 @@ var SceneBinaryGame = new Phaser.Class({
 	{
 		this.load.image('button0', 'assets/button0.png');
 		this.load.image('button1', 'assets/button1.png');
+		this.load.image('buttonClear', 'assets/button_clear.png');
 	},
 
 	create: function ()
 	{
-		var inputVal = "> ";
+		if (localStorage.getItem("inputVal") == null){
+			inputVal = "> ";	//first played, init
+		}
+		else{
+			var inputVal = localStorage.getItem("inputVal");
+		}
+
 		var btn0 = this.add.image(400, 600, 'button0');
 		var btn1 = this.add.image(1000, 600, 'button1');
+		var btnC = this.add.image(700, 600, 'buttonClear');
 		var Text2Send = this.add.text(700, 200, inputVal,{'backgroundColor':'#0f0', 'fontSize': '40px', 'color': '#00f'});
 
 		btn0.on('pointerdown', function (event) {
             inputVal += "0";
 			Text2Send.setText(inputVal);
-			// console.log(inputVal);
+			localStorage.setItem("inputVal", inputVal);
 		}, this);
         btn1.on('pointerdown', function (event) {
 			inputVal += "1";
 			Text2Send.setText(inputVal);
-			// console.log(inputVal);			
+			localStorage.setItem("inputVal", inputVal);
+		}, this);
+		btnC.on('pointerdown', function (event) {
+			inputVal = "> ";
+			Text2Send.setText(inputVal);
+			localStorage.setItem("inputVal", inputVal);
 		}, this);
 		btn0.setInteractive({ cursor: 'pointer' })
 		btn1.setInteractive({ cursor: 'pointer' })
+		btnC.setInteractive({ cursor: 'pointer' })
 	}
 
 });
